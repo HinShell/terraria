cf57ef7fd50b7c369ad1f4ba334757267f3cf028
HinShell <humanoidinshell@gmail.com>
2019-08-08 10:35:51 +0200

    lint run-deployTmodloader.sh

--- build/run-deployTmodloader.sh
+++ build/run-deployTmodloader.sh
@@ -9,18 +9,13 @@ if [ ! -e "${SERVER}" ] || [ ! -e "${VERSIONCHECK}" ] || [ -n "${FORCE_REDOWNLOA
   wget -q http://terraria.org/server/terraria-server-"${SERVER_VERSION}".zip -O /tmp/"${SERVER_ZIP}"
   bsdtar --strip-components=2 -xvf /tmp/"${SERVER_ZIP}" "${SERVER_VERSION}"/Linux/*
   rm "/tmp/${SERVER_ZIP}"
-  cd "${SERVER_ROOT}" || exit 1
-  chmod a+rw ./*
-  chmod a+x TerrariaServer*
   wget -q https://github.com/tModLoader/tModLoader/releases/download/v"${TMODLOADER_VERSION}"/tModLoader.Linux.v"${TMODLOADER_VERSION}".zip -O /tmp/"${TMODLOADER_ZIP}"
-  cd "${SERVER_ROOT}" || exit 1
   bsdtar -xvf /tmp/"${TMODLOADER_ZIP}"
   rm  /tmp/"${TMODLOADER_ZIP}"
-  cd "${SERVER_ROOT}" || exit 1
-  chmod a+rw ./*
-  chmod a+x tModLoaderServer* 
+  chmod a+rw "${SERVER_ROOT:?}"/*
+  chmod a+x "${SERVER_ROOT:?}"/TerrariaServer*
+  chmod a+x "${SERVER_ROOT:?}"/tModLoaderServer* 
   touch "${VERSIONCHECK}"
   chown -R terraria:terraria "${SERVER_ROOT}"
 fi
-#exec gosu terraria /launch.sh "$@"
 exec /run-enablemodTmodloader.sh "$@"
