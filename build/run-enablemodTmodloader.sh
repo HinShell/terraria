5829307ed857ad968f68604c078e7350db98348b
HinShell <humanoidinshell@gmail.com>
2019-08-11 13:33:26 +0200

    Check for tmod files before create json file

--- build/run-enablemodTmodloader.sh
+++ build/run-enablemodTmodloader.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-for mod in /mods/*.tmod
-do
-  lmod="${lmod}\"$(basename "${mod%.*}")\","
-done
+if [ "$(ls -A /mods/*.tmod 2>/dev/null)" ];then
+  for mod in /mods/*.tmod
+  do
+    lmod="${lmod}\"$(basename "${mod%.*}")\","
+  done
 
-lmod="[${lmod%?}]"
-echo "${lmod}" > /mods/enabled.json
-chown -R terraria:terraria /mods/enabled.json
-
+  lmod="[${lmod%?}]"
+  echo "${lmod}" > /mods/enabled.json
+  chown -R terraria:terraria /mods/enabled.json
+else
+  rm -f /mods/enabled.json
+fi
 exec gosu terraria /launch.sh "$@"
